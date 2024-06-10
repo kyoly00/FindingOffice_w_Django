60ab7dc3ad6f6ed76946a4d0865e4f60ad602048
kyoly00-panda <130558912+kyoly00-panda@users.noreply.github.com>
2024-06-10 13:02:17 +0900

    중간지점

--- home/views.py
+++ home/views.py
@@ -4,7 +4,7 @@ from .forms import SignUpForm, LoginForm
 from django.utils import timezone
 from django.contrib import messages
 from django.contrib.auth.hashers import make_password, check_password
-from .models import Customer, Reservation
+from .models import Customer, Reservation, ShareOffice
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.decorators import login_required
 
@@ -14,6 +14,15 @@ from urllib.request import Request
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 import json
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+Client_ID = os.getenv('CLIENT_ID')
+Client_KEY = os.getenv('CLIENT_KEY')
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -61,6 +70,62 @@ def my_page(request):
         return redirect('login')  # 고객 정보가 없으면 로그인 페이지로 리디렉션
 
     return render(request, 'mypage.html', {'customer': customer})
+
+def range_limit(latitudes, longitudes):
+    max_lat, max_lng = max(latitudes), max(longitudes)
+    min_lat, min_lng = min(latitudes), min(longitudes)
+    return max_lat, max_lng, min_lat, min_lng
+
+def call
+
+def finding_optimal(origin):
+    # 이동 시간을 저장할 딕셔너리 초기화
+    travel_times = {i: [] for i in range(1, len(location_dicts) + 1)}
+
+    # 각 위치 쌍에 대해 Google Maps Directions API를 호출하여 이동 시간 계산
+    for j, loc in enumerate(location_dicts):
+        so_lat = loc['so_lat']
+        so_lng = loc['so_lng']
+
+        # URL 파라미터 인코딩
+        params = {
+            'origin': f"{origin[0]},{origin[1]}",
+            'destination': f"{so_lat},{so_lng}",
+            'mode': how_to_go,
+            'transit_routing_preference': 'fewer_transfers',
+            'key': Client_Secret
+        }
+        endpoint = "https://maps.googleapis.com/maps/api/directions/json?"
+        url = endpoint + urllib.parse.urlencode(params)
+
+        response = urllib.request.urlopen(url).read()
+        response_json = json.loads(response)
+
+        # 이동 시간 추출
+        if response_json["status"] == "OK":
+            travel_time_seconds = response_json["routes"][0]["legs"][0]["duration"]["value"]
+            # 각 이동 시간에 대한 정보를 딕셔너리에 저장
+            travel_times[j + 1].append({
+                "so_id": loc['so_id'],  # 목적지 id
+                "travel_time_seconds": travel_time_seconds  # 이동 시간 (초)
+            })
+        else:
+            print("Error:", response_json["status"])
+            print("URL:", url)  # 오류가 발생한 URL 출력
+            print("Response:", response_json)  # 응답 출력
+
+    # 목적지별 총 이동 시간을 계산
+    total_travel_times = {loc['so_id']: 0 for loc in location_dicts}
+
+    for i in travel_times:
+        for time_info in travel_times[i]:
+            so_id = time_info['so_id']
+            travel_time_seconds = time_info['travel_time_seconds']
+            total_travel_times[so_id] += travel_time_seconds
+
+    # 총 이동 시간이 작은 10개의 so_id를 반환
+    sorted_so_ids = sorted(total_travel_times, key=total_travel_times.get)[:10]
+    print("Top 10 SO IDs with minimum total travel time:", sorted_so_ids)
 
 def location_view(request):
     return render(request, 'location.html')
